46745896958588da983a3d95ec62c71dee85e1ae
sonenhur <148170713+sonenhur@users.noreply.github.com>
2023-12-21 12:42:27 +0900

    Add files via upload

---
+++ src.main.webapp/14MVCBoard/테이블생성.sql
@@ -0,0 +1,19 @@
+--create table mvcboard (
+--idx int primary key auto_increment,
+--name varchar(50) not null,
+--title varchar(200) not null,
+--content varchar(2000) not null,
+--postdate date default (curdate()) not null,
+--ofile varchar(200),
+--sfile varchar(30),
+--downcount int(5) default 0 not null,
+--pass varchar(50) not null,
+--visitcount int default 0 not null);
+--
+--insert into mvcboard(name, title, content, pass) values ('김유신', '자료실 제목1 입니다.', '내용', '1234');
+--insert into mvcboard(name, title, content, pass) values ('장보고', '자료실 제목2 입니다.', '내용', '1234');
+--insert into mvcboard(name, title, content, pass) values ('이순신', '자료실 제목3 입니다.', '내용', '1234');
+--insert into mvcboard(name, title, content, pass) values ('강감찬', '자료실 제목4 입니다.', '내용', '1234');
+--insert into mvcboard(name, title, content, pass) values ('대조영', '자료실 제목5 입니다.', '내용', '1234');
+--
+--commit;
