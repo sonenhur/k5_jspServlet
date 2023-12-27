fabef8842e0e0cce5b2953fe5f969df6e984a9fe
sonenhur <148170713+sonenhur@users.noreply.github.com>
2023-12-27 17:38:45 +0900

    Update JDBConnect.java

--- src.main.java/common/JDBConnect.java
+++ src.main.java/common/JDBConnect.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 //import java.sql.PreparedStatement;
 //import java.sql.ResultSet;
 //import java.sql.Statement;
-
 import jakarta.servlet.ServletContext;
 
 public class JDBConnect {
